39c8f014a1b0421d6845d522a2e01314938a3abd
UngsterHamdung <0123ox@naver.com>
2023-02-13 10:43:30 +0900

    initial_robot

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/robot/catkin_ws/src
+# Build directory: /home/robot/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("urg_c")
+subdirs("laser_proc")
+subdirs("urg_node")
